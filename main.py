aee7c356a65f704dc5259c28be1e19b8cc301731
Qwark <qwark777@gmail.com>
2025-02-24 23:47:15 +0300

    some message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from ucimlrepo import fetch_ucirepo
 import pandas as pd
 automobile = fetch_ucirepo(id=10)
-# price continuous написано на сайте https://archive.ics.uci.edu/dataset/10/automobile
+# continuous from 5118 to 45400 написано на сайте https://archive.ics.uci.edu/dataset/10/automobile
 data = pd.DataFrame(automobile.data)
+
 data.to_csv('automobile_data.csv', index=False)
